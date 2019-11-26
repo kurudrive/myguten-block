3a5e807d1d6499df3c089fd8aa33ce869d7950b8
kurudrive <kurudrive@gmail.com>
2019-11-26 23:55:20 +0900

    phpcs

--- myguten-plugin.php
+++ myguten-plugin.php
@@ -4,15 +4,15 @@ Plugin Name: MyGuten Plugin
 */
 
 
-require_once('src/fancy-quote/block.php');
+require_once( 'src/fancy-quote/block.php' );
 
 // 依存ファイルのパスを登録
-$asset_file = include( plugin_dir_path( __FILE__ ) . 'build/index.asset.php');
+$asset_file = include( plugin_dir_path( __FILE__ ) . 'build/index.asset.php' );
 
 // ブロックの読み込み時に依存ファイル情報も読み込む
 wp_register_script(
-    'myguten-block',
-    plugins_url( 'build/index.js', __FILE__ ),
-    $asset_file['dependencies'],
-    $asset_file['version']
+	'myguten-block',
+	plugins_url( 'build/index.js', __FILE__ ),
+	$asset_file['dependencies'],
+	$asset_file['version']
 );
